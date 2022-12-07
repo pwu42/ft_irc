e8454cfccfb134fc2baf58c085535066e924f04f
Pascal WU <pwu@student.42.fr>
2022-12-07 15:56:51 +0100

    uppdate privmsg

--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -3,14 +3,13 @@
 void Server::cmdPrivmsg(Client * sender, SplitMsg & message)
 {
 	IMsgTarget * target;
-	std::string err;
 
 	if (message.getParams().size() < 1)
 		message.addReply(':' + hostname + ' ' + ERR_NORECIPIENT + ' ' + sender->getNick() + ' ' + replies[ERR_NORECIPIENT], sender);
 	else if (message.getParams().size() < 2)
 		message.addReply(':' + hostname + ' ' + ERR_NOTEXTTOSEND + ' ' + sender->getNick() + ' ' + replies[ERR_NOTEXTTOSEND], sender);
-	else if (!(target = findTarget(message.getParams()[0]))) // or channel not found
-		message.addReply(':' + hostname + ' ' + ERR_NOSUCHNICK + ' ' + sender->getNick() + ' ' + err + ' ' + replies[ERR_NOSUCHNICK], sender);
-	else
+	else if (!(target = findTarget(message.getParams()[0])))
+		message.addReply(':' + hostname + ' ' + ERR_NOSUCHNICK + ' ' + sender->getNick() + ' ' + message.getParams()[0] + ' ' + replies[ERR_NOSUCHNICK], sender);
+	else // send to client/channel
 		message.addReply(':' + sender->getFullName() + ' ' + message.getCommand() + ' ' + target->getName() + " :" + message.getParams()[1] + "\r\n", target);
 }
